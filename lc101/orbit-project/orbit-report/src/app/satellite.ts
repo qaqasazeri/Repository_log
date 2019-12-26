1cf5efb56819e1b0c5a1c8f0373bcda32ac7c86c
Fagan Safarov <fagansafarov1985@gmail.com>
2019-12-26 01:33:34 -0600

    till part 6

---
+++ lc101/orbit-project/orbit-report/src/app/satellite.ts
@@ -0,0 +1,27 @@
+export class Satellite {
+
+name: string;
+orbitType: string;
+type: string;
+operational: boolean;
+launchDate: string;
+alternateColor: string = 'red';
+
+constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean)
+ { 
+     this.name=name;
+     this.orbitType=orbitType;
+     this.type=type;
+     this.operational=operational;
+     this.launchDate=launchDate;
+     
+ }
+shouldShowWarning() :boolean{
+
+    if(this.type==="Space Debris"){
+        return true;
+    }
+    return false;
+}
+
+}
